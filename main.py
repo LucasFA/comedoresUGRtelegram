1cbbe4973a04281ec6f381c35f9edc0288cc2885
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-10-28 00:37:47 +0100

    basic bot

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-from telegram.ext import Application
+import os
+from dotenv import load_dotenv
+from telegram import Update, ForceReply
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
 
 def main():
-    print("Hello from comedoresugrtelegram!")
-    application = Application.builder().token(token).concurrent_updates(True).read_timeout(30).write_timeout(30).build()
-    application.run_polling()
+    load_dotenv()
+    token = os.getenv("TELEGRAM_TOKEN")
+    if not token:
+        raise RuntimeError("Telegram token not defined")
+
+    app = ApplicationBuilder().token(token).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(CommandHandler("help", help))
+
+    app.run_polling()
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_text(
+        rf"Hola {user.mention_html()}! Todavía estoy en desarrollo. Vuelve más tarde!"
+    )
+
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
 
 
 if __name__ == "__main__":
